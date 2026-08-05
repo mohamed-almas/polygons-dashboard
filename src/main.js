47e95f21d0c97985c8b3313e8fc8581d526755c4
Mohamed Almas <maaziz@yahoo.com>
2026-08-05 14:26:29 +0400

    feat: add KPI cards fed by country/region aggregate matviews

--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
 import './style.css'
 import { initMap } from './map.js'
+import { renderKpiCards } from './kpi.js'
 
 const { setLevel } = await initMap('map')
 await setLevel('port')
+await renderKpiCards('global')
 
 document.getElementById('level-select').addEventListener('change', (e) => {
   setLevel(e.target.value)
+})
+
+document.getElementById('scope-select').addEventListener('change', (e) => {
+  if (e.target.value === 'global') renderKpiCards('global')
+  // region/country drill-down group_key selection is a future enhancement;
+  // for now, global is the only fully wired scope.
 })
